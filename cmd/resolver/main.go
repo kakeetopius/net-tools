0ac413dda3f2ff64cd8e2dd1431eba1a4dc9ff9a
kakeetopius <pius.kakeeto@strathmore.edu>
2026-03-21 21:57:37 +0300

    Update resolver

--- cmd/resolver/main.go
+++ cmd/resolver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
@@ -12,13 +13,14 @@ import (
 )
 
 type Result struct {
-	Query   string
-	Answers []string
-	Err     error
+	Query   string   `json:"query"`
+	Answers []string `json:"answers"`
+	Err     string   `json:"error,omitempty"`
 }
 
 type Options struct {
 	ReverseLookup bool
+	PrintJSON     bool
 }
 
 func main() {
@@ -39,7 +41,10 @@ func main() {
 		fmt.Println("Error: ", err)
 		return
 	}
-	printResults(results)
+	err = printResults(results, &opts)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
 
 func resolve(queries []string) ([]Result, error) {
@@ -53,10 +58,14 @@ func resolve(queries []string) ([]Result, error) {
 
 	for _, name := range queries {
 		addresses, err := resolver.LookupHost(ctx, name)
+		var errStr string
+		if err != nil {
+			errStr = err.Error()
+		}
 		results = append(results, Result{
 			Query:   name,
 			Answers: addresses,
-			Err:     err,
+			Err:     errStr,
 		})
 	}
 	return results, nil
@@ -72,42 +81,52 @@ func reverseLookup(queries []string) ([]Result, error) {
 
 	for _, addr := range queries {
 		names, err := resolver.LookupAddr(ctx, addr)
+		var errStr string
+		if err != nil {
+			errStr = err.Error()
+		}
 		results = append(results, Result{
 			Query:   addr,
 			Answers: names,
-			Err:     err,
+			Err:     errStr,
 		})
 	}
 	return results, nil
 }
 
-func printResults(results []Result) {
+func printResults(results []Result, opts *Options) error {
+	if opts.PrintJSON {
+		jsonResults, err := json.MarshalIndent(results, "", "  ")
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(jsonResults))
+		return nil
+	}
+
 	data := pterm.TableData{{"Query", "Response(s)", "Errors"}}
 	for _, result := range results {
 		answerString := strings.Builder{}
 		for _, answer := range result.Answers {
 			fmt.Fprintf(&answerString, "%v\n", answer)
 		}
-		var errorStr string
-		if result.Err != nil {
-			errorStr = result.Err.Error()
-		} else {
-			errorStr = "None"
-		}
-		data = append(data, []string{result.Query, answerString.String(), errorStr})
+		data = append(data, []string{result.Query, answerString.String(), result.Err})
 	}
 
 	pterm.DefaultTable.WithHasHeader(true).WithBoxed(true).WithRowSeparator("-").WithHeaderRowSeparator("-").WithData(data).Render()
+	return nil
 }
 
 func parseArgs() ([]string, Options, error) {
 	flagSet := pflag.NewFlagSet("resolver", pflag.ExitOnError)
 	reverseLookup := flagSet.BoolP("reverse", "r", false, "Perform a reverse lookup for the given IP(s).")
+	json := flagSet.BoolP("json", "j", false, "Output the results in json form")
 	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
 		return nil, Options{}, err
 	}
 	return flagSet.Args(), Options{
 		ReverseLookup: *reverseLookup,
+		PrintJSON:     *json,
 	}, nil
 }
